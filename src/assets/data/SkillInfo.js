b67c820d5c661cc4f8f71e4f18859a3e2002dcf2
Oscar La <67846380+oscarlaaaa@users.noreply.github.com>
2022-01-18 20:48:36 -0800

    java/typescript

---
+++ src/assets/data/SkillInfo.js
@@ -0,0 +1,24 @@
+
+const proficiencies = [
+  { name: "Java", icon: "https://i.imgur.com/In3h17w.png", type: "language", comfort: 3 },
+  { name: "HTML/CSS", icon: "https://i.imgur.com/ZpX7b8x.png",  type: "language", comfort: 3 },
+  { name: "Java/Typescript", icon: "https://i.imgur.com/C8t2E5k.png",  type: "language", comfort: 3 },
+  { name: "Python", icon: "https://i.imgur.com/y3BsWEq.png?1", type: "language", comfort: 3 },
+  { name: "SQL", icon: "https://i.imgur.com/SPaHNHC.png",  type: "language", comfort: 2 },
+  
+  { name: "React.js", icon: "https://i.imgur.com/tNQCFxZ.png",  type: "framework/engine", comfort: 3 },
+  { name: "FastAPI", icon: "https://i.imgur.com/xfaNoyV.png", type: "framework/engine", comfort: 3 },
+  { name: "Node.js", icon: "https://i.imgur.com/IfGwZal.png", type: "framework/engine", comfort: 2 },
+  { name: "Express.js", icon: "https://i.imgur.com/IfGwZal.png", type: "framework/engine", comfort: 2 },
+  { name: "Bootstrap", icon: "https://i.imgur.com/WkXyaK6.png",  type: "framework/engine", comfort: 2 },
+  { name: "JUnit", icon: "https://i.imgur.com/H6kViaM.png",  type: "framework/engine", comfort: 2 },
+  { name: "Unity", icon: "https://i.imgur.com/4HU8Pew.png",  type: "framework/engine", comfort: 1 },
+
+  { name: "MySQL", icon: "https://i.imgur.com/7P9OyM0.png",  type: "server/db", comfort: 3 },
+  { name: "Google Firebase", icon: "https://i.imgur.com/p1o2pN3.png",  type: "server/db", comfort: 2 },
+  { name: "Google Firestore", icon: "https://i.imgur.com/p1o2pN3.png",  type: "server/db", comfort: 2 },
+  { name: "AWS", icon: "https://i.imgur.com/bZy2I43.png",  type: "server/db", comfort: 2 },
+  { name: "PostgreSQL", icon: "https://i.imgur.com/XrAjylL.png",  type: "server/db", comfort: 1 },
+];
+
+export default proficiencies;
